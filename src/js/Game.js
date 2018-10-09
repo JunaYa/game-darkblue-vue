075d95d8ade3f6a85fe9ef523882f0b4e3854d67
Yajun Wang <ayac3j@gmail.com>
2018-10-09 11:39:08 +0800

    extract code in game

---
+++ src/js/Game.js
@@ -0,0 +1,67 @@
+import Level from "../js/Level.js";
+import State from "../js/State.js";
+import DOMDisplay from "../js/DOMDisplay.js";
+
+function trackKeys(keys) {
+    let down = Object.create(null);
+    function track(event) {
+        if (keys.includes(event.key)) {
+            down[event.key] = event.type == "keydown";
+            event.preventDefault();
+        }
+    }
+    window.addEventListener("keydown", track);
+    window.addEventListener("keyup", track);
+    return down;
+}
+
+const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+function runAnimation(frameFunc) {
+    let lastTime = null;
+    function frame(time) {
+        let stop = false;
+        if (lastTime != null) {
+            let timeStep = Math.min(time - lastTime, 100) / 1000;
+            if (frameFunc(timeStep) === false) return;
+        }
+        lastTime = time;
+        requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+function runLevel(parent, level) {
+    let display = new DOMDisplay(parent, level);
+    let state = State.start(level);
+    let ending = 1;
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time, arrowKeys);
+            display.setState(state);
+            if (state.status == "playing") {
+                return true;
+            } else if (ending > 0) {
+                ending -= time;
+                return true;
+            } else {
+                display.clear();
+                resolve(state.status);
+                return false;
+            }
+        });
+    });
+}
+
+async function run(parent, plans) {
+    for (let level = 0; level < plans.length; ) {
+        let status = await runLevel(parent, new Level(plans[level]));
+        if (status == "won") {
+            level++;
+        }
+    }
+}
+
+export default {
+    run,
+}
